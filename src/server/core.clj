4c53a59530a7d657cb97d7490abee54e301e5d81
MMz <maarten.metz@philips.com>
2019-06-28 15:36:39 +0200

    Add a world atom on the REST API

--- src/server/core.clj
+++ src/server/core.clj
@@ -7,15 +7,48 @@
 ; simple atom for exposing a global function so the server can close itself
 (def server (atom nil))
 
+(def new-world {:count 0})
+
+(def world (atom {:count 0}))
+
+(defn world-resource
+  []
+  "Modifiable world. Look at yada.resources.atom-resource for ways to add
+  last-modified headers and the like"
+  (let [state world]
+    (yada/resource
+      {:methods {:get  {:produces "application/json"
+                        :response (fn [ctx] @state)}}})))
+
+(defn update-world-resource
+  []
+  "Modifiable world. Look at yada.resources.atom-resource for ways to add
+  last-modified headers and the like"
+  (let [state world]
+    (yada/resource
+     {:methods {:post
+                {:response (fn [ctx]
+                             (swap! state update-in [:count] inc))}}})))
+
+(defn reset-world-resource
+  []
+  "Modifiable world. Look at yada.resources.atom-resource for ways to add
+  last-modified headers and the like"
+  (let [state world]
+    (yada/resource
+     {:methods {:post
+                {:response (fn [ctx]
+                             (reset! world new-world))}}})))
+
 (defn routes []
   ["/"
    {
-    "hello" (yada/as-resource "Hello World!")
-    "json"  (yada/as-resource {:message "yo!"})
-    "world" (yada/as-resource tb/world)
-    "die"   (yada/as-resource (fn []
-                                (future (Thread/sleep 100) (@server))
-                                "shutting down in 100ms..."))}])
+    "world"  (world-resource)
+    "update" (update-world-resource)
+    "reset"  (reset-world-resource)
+    "die"     (yada/as-resource (fn []
+                                  (future (Thread/sleep 100) (@server))
+                                  "shutting down in 100ms..."))}])
 
 (defn run []
   (let [listener (yada/listener (routes) {:port 3000})
